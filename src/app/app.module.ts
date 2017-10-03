70208f398a1c67ac320260bc1897c2733c46d6d4
Marques <j.marques@accenture.com>
2017-10-03 16:16:43 -0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CalendarModule } from 'angular-calendar';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CalendarModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
