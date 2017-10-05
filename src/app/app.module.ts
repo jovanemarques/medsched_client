f162781fd632ceb5b0bf3d79b67a580a727fb854
Marques <j.marques@accenture.com>
2017-10-05 11:19:38 -0300

    sidemenu and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { CalendarModule } from 'angular-calendar';
+
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CalendarModule.forRoot()
   ],
   providers: [],
